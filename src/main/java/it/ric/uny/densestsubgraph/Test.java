61f6047040df88864e1764d56d42be97184eb9a6
Riccardo Orlando <orlandoricc@gmail.com>
2017-11-25 12:37:41 +0100

    Things

--- src/main/java/it/ric/uny/densestsubgraph/Test.java
+++ src/main/java/it/ric/uny/densestsubgraph/Test.java
@@ -1,27 +1,41 @@
 package it.ric.uny.densestsubgraph;
 
 import com.google.common.graph.MutableGraph;
+import com.sun.xml.internal.ws.api.streaming.XMLStreamWriterFactory;
+import org.jgrapht.Graph;
+import org.jgrapht.graph.DefaultEdge;
 
 import java.io.IOException;
+import java.util.SortedMap;
+import java.util.concurrent.ForkJoinPool;
 
 public class Test {
 
     public static void main(String[] args) {
 
-        MutableGraph<Integer> graph = null;
+        MutableGraph<Integer> graphGuava = null;
         //String filename = "data/dummy_graph.txt";
-        String filename = "data/facebook_combined.txt";
-
-        try {
-            graph = GraphParser.parseGuava(filename);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        //String filename = "data/facebook_combined.txt";
+        //String filename = "data/ca-AstroPh.txt";
+        String filename = "data/roadNet-CA.txt";
+        SortedMap<Integer, Integer> map = GraphParser.toMap(filename);
 
         long startTime = System.currentTimeMillis();
-        int degree = graph.degree(0);
+        Graph<Integer, DefaultEdge> graph = ForkJoinPool.commonPool().invoke(new UndirectedGraph(map));
         long endTime = System.currentTimeMillis();
+
         double time = endTime - startTime;
+        System.out.println("Parallel Time: " + time + " ms");
+
+        //int degree = graph.degree(0);
+        startTime = System.currentTimeMillis();
+        GraphParser.parseGuava(map);
+        //graph = GraphParser.parseGuava(filename);
+
+        endTime = System.currentTimeMillis();
+
+        time = endTime - startTime;
+
         System.out.println("Guava Time: " + time + " ms");
     }
 }
--- src/main/java/it/ric/uny/densestsubgraph/UndirectedGraph.java
+++ src/main/java/it/ric/uny/densestsubgraph/UndirectedGraph.java
@@ -1,17 +1,62 @@
 package it.ric.uny.densestsubgraph;
 
-import com.google.common.graph.MutableGraph;
 
-import java.util.HashMap;
-import java.util.HashSet;
+import org.jgrapht.Graph;
+import org.jgrapht.Graphs;
+import org.jgrapht.graph.DefaultEdge;
+import org.jgrapht.graph.Pseudograph;
 
-public class UndirectedGraph {
+import java.util.SortedMap;
+import java.util.concurrent.RecursiveTask;
 
-    private MutableGraph<Integer> graph;
-    private HashMap<Integer, Integer> degreeMap;
+public class UndirectedGraph extends RecursiveTask<Graph<Integer, DefaultEdge>>{
 
-    public UndirectedGraph(MutableGraph<Integer> graph) {
-        //this.graph = graph;
-        //this.degreeMap = graph.nodes().stream().forEach(x -> );
+    private SortedMap<Integer, Integer> rows;
+    private int low;
+    private int high;
+    private int cutoff;
+
+    public UndirectedGraph(SortedMap<Integer, Integer> rows) {
+        this.rows = rows;
+        this.high = this.rows.keySet().size();
+        this.cutoff = this.high/16;
+    }
+
+    public UndirectedGraph(SortedMap<Integer, Integer> rows, int high, int cutoff) {
+        this.rows = rows;
+        this.high = high;
+        this.cutoff = cutoff;
+    }
+
+    @Override
+    protected Graph<Integer, DefaultEdge> compute() {
+        Graph<Integer, DefaultEdge> g = new Pseudograph<>(DefaultEdge.class);
+
+        if (high - low < cutoff) {
+            for (Integer x : rows.keySet()){
+                int n1 = x;
+                int n2 = rows.get(x);
+                g.addVertex(n1);
+                g.addVertex(n2);
+                g.addEdge(n1,n2);
+            }
+
+            return g;
+        }
+
+        SortedMap<Integer, Integer> lowList = rows.subMap(low,(high+low)/2);
+        SortedMap<Integer, Integer> highList = rows.subMap((high+low)/2, high);
+        //List<String> highList = rows.stream().skip((high+low)/2).collect(Collectors.toList());
+
+        UndirectedGraph left = new UndirectedGraph(lowList, lowList.keySet().size(), this.cutoff);
+        UndirectedGraph right = new UndirectedGraph(lowList, highList.keySet().size(), this.cutoff);
+
+        left.fork();
+        g = right.compute();
+        Graph<Integer, DefaultEdge> gLeft = left.join();
+
+        Graphs.addGraph(g, gLeft);
+
+        return g;
     }
 }
